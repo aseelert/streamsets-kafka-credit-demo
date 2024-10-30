93ea0aa3050441ba932e91f39612273719d5a7a7
Alexander Seelert <alexander@de.ibm.com>
2024-10-30 16:17:44 +0100

    add history postgres

---
+++ docker/init-history.sql
@@ -0,0 +1,22 @@
+-- init.sql
+CREATE TABLE IF NOT EXISTS financial_transactions_history (
+    firstname VARCHAR(50),
+    lastname VARCHAR(50),
+    name VARCHAR(100),
+    birthdate DATE,
+    email VARCHAR(100),
+    city VARCHAR(100),
+    state CHAR(2),  -- US state abbreviation
+    address TEXT,
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    country VARCHAR(50),
+    customer_number INT,
+    transaction_id BIGINT,
+    account_number VARCHAR(20),
+    account_type VARCHAR(20),
+    amount DECIMAL(10, 2),
+    timestamp TIMESTAMP,
+    type VARCHAR(10),
+    risk_score INT
+);
